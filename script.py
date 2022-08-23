3371a2c5690010cdb25dad6045158b389a16375a
WilderGitHub <wilder_serdan@hotmail.com>
2022-08-22 20:20:39 -0400

    faltan

--- script.py
+++ script.py
@@ -1,33 +1,74 @@
 import requests
 import codecs
+import html
 from bs4 import BeautifulSoup
-proxies = {"http": "http://10.1.11.50:8080",
-           "https": "http://10.1.11.50:8080"}
+from bs4 import UnicodeDammit
+import re
+import html
+import pandas as pd
+from requests import get
 
-r=requests.get("https://www.la-razon.com/economia/1/", proxies=proxies)
-r=r.content.decode('utf-8','ignore')
+proxies = {"http": "http://10.1.11.50:8080","https": "http://10.1.11.50:8080"}
+
+r=requests.get("https://www.la-razon.com/economia/page/1/", proxies=proxies)
+r=r.content.decode('windows-1252','ignore')
 soup = BeautifulSoup(r, 'html5lib')
 ###print("Encoding method :", soup.original_encoding)
-''' divis = soup.find_all('div')
-for divi in divis:
-    print(divi)
- '''
-###divisc = soup.find_all('div',class_='articles')#,limit=1)
-''' for divi in divisc:
-    if 'id' in divi.attrs:
-        print("el id es: ", divi['id'])
- '''
-''' pisis = soup.find_all('p')
-for pis in pisis:
-    print(pis) '''
-arts = soup.find('div',id='lr-main')#,limit=1)
-''' for art in arts:
-    if 'a' in art.attrs:
-        print("el a es: ", art['a'])
- '''
-aa = arts.find_all('a',class_="link")
-for a in aa:
-    if 'href' in a.attrs:
-        print("el a es: ", a['href'])
-    #print(a.href.text)
 
+###################################
+### Encontramos el cuerpo principal de los articulos
+arts = soup.find('div',id='lr-main')
+# Encontramos los links de los articulos
+links = arts.find_all('a',class_="link")
+listalinks=[]
+for a in links:
+    if 'href' in a.attrs:
+        ##print("el a es: ", a['href'])
+        listalinks.append(a['href'])
+    #print(a.href.text)
+####print("esta seriya la lista: ",listalinks)
+print("numero de links", len(links))
+############################
+### Encontramos el id de cada articulo
+for n in range(len(links)): 
+    print("---- inicio ---------  ", n)
+    r=requests.get(listalinks[n], proxies=proxies)
+    r=r.content.decode('windows-1252',errors='ignore')
+    articulos = BeautifulSoup(r, 'html5lib')
+    elarticulo = articulos.find('div',id="lr-main")
+    
+    ids=[]
+    
+    indice=elarticulo.find_next(id)
+    #print("el id es: ",indice)
+    # indices=elarticulo.find_all(id)#,limit=4) #sabemos que esta en un cuerto nivel
+    #for id in indice:
+    print("indicesdsd",indice.id)
+    if 'id' in indice.attrs:
+        print("los id de los divs son: ",indice['id'])
+    #id=id['id']
+    #print("el id es: ",id) 
+    #ids=[]
+
+    titular=elarticulo.find_next("h1").text#, id="article679331")
+    titulares=[]
+    titulares.append(titular)
+    print("el titular es: ", titulares)
+    #######  Encontramos el texto
+    detalle = elarticulo.find_next('p').text
+
+    detalles=[]
+    detalles.append(detalle)
+    print("el detalle es: ", detalles)
+
+    divsparrafos = elarticulo.find_next('div',class_="article-body")
+
+    print("el parrafo es:")
+
+    parrafos=divsparrafos.findChildren('p')
+    parrafoses=[]
+    for pp in parrafos:
+        parrafoses.append(pp)
+        unparrafo=' '.join(map(str,parrafoses)).replace("</p> <p>"," ").replace("<p>","").replace("</p>","")
+    print(unparrafo)
+    print("---- fin ---------")
