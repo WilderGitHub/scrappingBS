ddc801b01689cd065b6df9e0d3cde66156a47264
wilder <wilder_serdan@hotmail.com>
2022-08-26 22:22:23 -0400

    empezando selenium

---
+++ eldeber.py
@@ -0,0 +1,33 @@
+# Librerías
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+# Opciones de navegación
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+
+driver_path = 'D:\instaladores\chromedriver_win32\\chromedriver.exe'
+
+driver = webdriver.Chrome(driver_path, chrome_options=options)
+
+# Iniciarla en la pantalla 2
+driver.set_window_position(2000, 0)
+driver.maximize_window()
+time.sleep(1)
+
+# Inicializamos el navegador
+driver.get('https://eldeber.com.bo/economia')
+# driver.get('https://eltiempo.es')
+
+#WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.linkText('/economia')))).click()
+# driver.quit()
+
+WebDriverWait(driver, 5)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '/html/body/div[1]/div/main/div[4]/div/div[3]/div/section/div/div[2]/a')))\
+    .click()
--- larazon.py
+++ larazon.py
@@ -12,123 +12,130 @@ from datetime import datetime as dt
 proxies = {"http": "http://10.1.11.50:8080",
            "https": "http://10.1.11.50:8080"}
 
-desde=801
-hasta=900
-pasito=1
-columnas=["codigo","fecha","titular","detalle","texto"]
-consolidado=pd.DataFrame()
-for pagina in range(desde,hasta+1,pasito):
+desde = 800
+hasta = 1000
+pasito = 1
+columnas = ["codigo", "fecha", "titular", "detalle", "texto"]
+consolidado = pd.DataFrame()
+for pagina in range(desde, hasta+1, pasito):
     url = "https://www.la-razon.com/economia/page/" + str(pagina)
-    r=requests.get(url, proxies=proxies)
-    #r=r.content.decode('windows-1252','ignore')
-    r=r.content.decode('utf-8','ignore')
+    r = requests.get(url)  # , proxies=proxies)
+    # r=r.content.decode('windows-1252','ignore')
+    r = r.content.decode('utf-8', 'ignore')
     soup = BeautifulSoup(r, 'html5lib')
     ###################################
-    ### Encontramos el cuerpo principal de los articulos
-    arts = soup.find('div',id='lr-main')
+    # Encontramos el cuerpo principal de los articulos
+    arts = soup.find('div', id='lr-main')
     # Encontramos los links de los articulos
-    links = arts.find_all('a',class_="link",limit=5)
+    links = arts.find_all('a', class_="link", limit=5)
     ''' for a in links:
         print("el link seriya este: ",a['href'])
      '''
-    listalinks=[]
+    listalinks = []
     for a in links:
         if 'href' in a.attrs:
             listalinks.append(a['href'])
-    #print("numero de links", len(links))
+    # print("numero de links", len(links))
     ############################
-    ### Encontramos el id de cada articulo
-    ids=[]
-    fechas=[]
-    titulares=[]
-    detalles=[]
-    textos=[]
-    for n in range(len(links)): 
-        #print("---- inicio ---------  ", n)
-        r=requests.get(listalinks[n], proxies=proxies)
-        r=r.content.decode('utf-8',errors='ignore')
-        articulos = BeautifulSoup(r, 'lxml')#html5lib')
-        elarticulo = articulos.find('div',id="lr-main")
-        ##print("el articulo: ", type(elarticulo))
-        #######  Encontramos el id
+    # Encontramos el id de cada articulo
+    ids = []
+    fechas = []
+    titulares = []
+    detalles = []
+    textos = []
+    for n in range(len(links)):
+        # print("---- inicio ---------  ", n)
+        r = requests.get(listalinks[n])  # , proxies=proxies)
+        r = r.content.decode('utf-8', errors='ignore')
+        articulos = BeautifulSoup(r, 'lxml')  # html5lib')
+        elarticulo = articulos.find('div', id="lr-main")
+        # print("el articulo: ", type(elarticulo))
+        # Encontramos el id
         try:
-            #print("tryando")
-            indice=elarticulo.find_next(id).find_next(id).find_next(id).find_next(id)
+            # print("tryando")
+            indice = elarticulo.find_next(id).find_next(
+                id).find_next(id).find_next(id)
             ids.append(indice['id'])
         except:
-            #print("exceptando")
-            indice=elarticulo.find_next(id)
-            #print("a ver",indice)
-            #ids.append(indice['id'])
+            # print("exceptando")
+            indice = elarticulo.find_next(id)
+            # print("a ver",indice)
+            # ids.append(indice['id'])
             ids.append("n.d.")
-            
+
         else:
-            #print("elseando")
-            indice=elarticulo.find_next(id).find_next(id).find_next(id).find_next(id)
+            # print("elseando")
+            indice = elarticulo.find_next(id).find_next(
+                id).find_next(id).find_next(id)
             ids.append(indice['id'])
-        #indice=elarticulo.find_all("id",limit=4)
-        #print("el id es: ",indice['id'])
-      #ids.append(indice['id'])
-      
-        #######  Encontramos la fecha
-        
-        locale.setlocale(locale.LC_TIME, '')
+        # indice=elarticulo.find_all("id",limit=4)
+        # print("el id es: ",indice['id'])
+      # ids.append(indice['id'])
+
+        # Encontramos la fecha
+
+        # , '') ESTO HAY QUE CAMBIAR
+        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
         try:
-            fecha=elarticulo.find_all('span',limit=2)[1].text    
+            fecha = elarticulo.find_all('span', limit=2)[1].text
             mifecha = dt.strptime(fecha, ' / %d de %B de %Y')
         except:
-            fecha=elarticulo.find_all('span',limit=1)[0].text  
+            fecha = elarticulo.find_all('span', limit=1)[0].text
             mifecha = "/ 17 de enero de 1977"
-           #mifecha = dt.strptime(fecha, ' / %d de %B de %Y')
-        else:#except ValueError:
-            fecha=elarticulo.find_all('span',limit=2)[1].text    
+           # mifecha = dt.strptime(fecha, ' / %d de %B de %Y')
+        else:  # except ValueError:
+            fecha = elarticulo.find_all('span', limit=2)[1].text
             mifecha = dt.strptime(fecha, ' / %d de %B de %Y')
-               #print("La fecha es: ",mifecha)
+            # print("La fecha es: ",mifecha)
         fechas.append(mifecha)
-        #######  Encontramos el titular
+        # Encontramos el titular
         try:
-            titular=elarticulo.find_next("h1").text
+            titular = elarticulo.find_next("h1").text
         except:
-            titular=elarticulo.find_next("p",class_="special-art-title")
-            #titular=elarticulo.find_next("h1").text
+            titular = elarticulo.find_next("p", class_="special-art-title")
+            # titular=elarticulo.find_next("h1").text
         else:
-            titular=elarticulo.find_next("h1").text
-        titulares.append(titular)    
-                    
-        #print("el titular es: ", titulares)
-        
-        #######  Encontramos el detalle
+            titular = elarticulo.find_next("h1").text
+        titulares.append(titular)
+
+        # print("el titular es: ", titulares)
+
+        # Encontramos el detalle
         detalle = elarticulo.find_next('p').text
         detalles.append(detalle)
-        #print("el detalle es: ", detalles)
-        #######  Encontramos el texto
+        # print("el detalle es: ", detalles)
+        # Encontramos el texto
         try:
-            #print("trai")
-            divsparrafos = elarticulo.findNext('div',class_="article-body")
-            parrafos=divsparrafos.findChildren('p')
+            # print("trai")
+            divsparrafos = elarticulo.findNext('div', class_="article-body")
+            parrafos = divsparrafos.findChildren('p')
         except:
-            #print("ezept")
-            divsparrafos = elarticulo.find_all('div',class_="article-body",limit=1)
+            # print("ezept")
+            divsparrafos = elarticulo.find_all(
+                'div', class_="article-body", limit=1)
             textos.append("no se consiguió el texto")
-            #parrafos=divsparrafos.findChildren('p')
+            # parrafos=divsparrafos.findChildren('p')
         else:
-            #print("elsita")
-            divsparrafos = elarticulo.findNext('div',class_="article-body")
-            parrafos=divsparrafos.findChildren('p')
-            #print("divsparrafitos tipo: ", type(divsparrafos))
-        #print("los divsparrafo seriya", divsparrafos)
-            parrafos=divsparrafos.findChildren('p')
-        #print("parrafitos: ", parrafos)
-        parrafoses=[]
+            # print("elsita")
+            divsparrafos = elarticulo.findNext('div', class_="article-body")
+            parrafos = divsparrafos.findChildren('p')
+            # print("divsparrafitos tipo: ", type(divsparrafos))
+        # print("los divsparrafo seriya", divsparrafos)
+            parrafos = divsparrafos.findChildren('p')
+        # print("parrafitos: ", parrafos)
+        parrafoses = []
         for pp in parrafos:
             parrafoses.append(pp.string)
-            unparrafo=' '.join(map(str,parrafoses))#.replace("</p> <p>"," ").replace("<p>","").replace("</p>","")
+            # .replace("</p> <p>"," ").replace("<p>","").replace("</p>","")
+            unparrafo = ' '.join(map(str, parrafoses))
         textos.append(unparrafo)
-    
-    troso=pd.DataFrame(list(zip(ids, fechas, titulares,detalles,textos)),columns=columnas)
-    
-    consolidado= consolidado.append(troso, ignore_index=True)
-#print("consolidado: ", consolidado.head(20))
-#consolidado.to_excel("prueba.xlsx", encoding='utf8')    
-consolidado.to_csv("prueba"+str(desde)+"-"+str(hasta)+".csv", encoding='utf8',sep="|")   
-print("Yasta terminado oe") 
+
+    troso = pd.DataFrame(
+        list(zip(ids, fechas, titulares, detalles, textos)), columns=columnas)
+
+    consolidado = consolidado.append(troso, ignore_index=True)
+# print("consolidado: ", consolidado.head(20))
+# consolidado.to_excel("prueba.xlsx", encoding='utf8')
+consolidado.to_csv("prueba"+str(desde)+"-"+str(hasta) +
+                   ".csv", encoding='utf8', sep="|")
+print("Yasta terminado oe")
